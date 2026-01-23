8e7ce16844ab8cc7fd4399f78d23b7491a8b050f
Chyngyzhan <axmed3000500@gmail.com>
2026-01-23 11:09:12 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from utils import say_hello, area_square,power,multiply,find_min
+
+say_hello("Chyngyzkhan")
+print(power())
+print(power(3,3))
+
+print(area_square(4))
+
+print(multiply(2,2))
+
+a = [1,2,3,4,5,6,7,8,9]
+print(find_min(a))
